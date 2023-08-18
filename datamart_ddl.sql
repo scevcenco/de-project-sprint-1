4c751d2d91493bff1de7370e1a3d5265952d00ea
Anastasia Shevchenko <a.scevcenco@gmail.com>
2023-08-18 15:41:28 +0400

    changed datamart_ddt, added comments.txt

--- datamart_ddl.sql
+++ datamart_ddl.sql
@@ -1,9 +1,8 @@
 
 
 CREATE TABLE analysis.dm_rfm_segments (
-	user_id int4 NOT NULL,
-	recency smallint not null,
-	frequency smallint not null,
-	monetary_value smallint not null,
-	CONSTRAINT users_pkey PRIMARY KEY (user_id)
+	user_id int4 NOT null primary key,
+	recency smallint not null check (recency >= 1 and recency <= 5),
+	frequency smallint not null check (frequency >= 1 and frequency <= 5),
+	monetary_value smallint not null check (monetary_value >= 1 and monetary_value <= 5)
 ); 
